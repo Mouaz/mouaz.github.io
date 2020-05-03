476a2a1d5086baf64cd7ab6a0c5a72d6b825c548
Mouaz Alabsawi <mouaz.alabsawi@gmail.com>
2020-05-03 21:45:02 +0200

    Changing some content

--- src/App.tsx
+++ src/App.tsx
@@ -8,15 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Mouaz was just trying <code>github pages</code>.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://pages.github.com/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Know more
         </a>
       </header>
     </div>
