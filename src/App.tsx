41e2b6c19678c19258e504b9e7ae7fc877fae794
Mouaz Alabsawi <mouaz.alabsawi@gmail.com>
2020-05-10 16:29:28 +0200

    Update with personal info

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+import Profile from './personal/profile';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
+    
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Mouaz was just trying <code>github pages</code>.
-        </p>
-        <a
-          className="App-link"
-          href="https://pages.github.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Know more
-        </a>
+    <Profile />
       </header>
     </div>
   );
