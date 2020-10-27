2437e140d43f7835da896c14addb29c58a01417d
Mouaz <mouaz.alabsawi@gmail.com>
2020-10-27 17:22:51 +0000

    Updates

---
+++ precache-manifest.a98a18ffe24373a8d6e1fd692d7b5852.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd09ee2729428d590f907743e0f6945d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "39e70ce7b89849193cd2",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "f1795aabdeb60811f9a7",
+    "url": "/static/css/main.31124b05.chunk.css"
+  },
+  {
+    "revision": "39e70ce7b89849193cd2",
+    "url": "/static/js/2.ac14ec9e.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.ac14ec9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1795aabdeb60811f9a7",
+    "url": "/static/js/main.2d71251a.chunk.js"
+  },
+  {
+    "revision": "8ab32378fe28b6ee7879",
+    "url": "/static/js/runtime-main.3788ad38.js"
+  }
+]);
